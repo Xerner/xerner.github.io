4f821fa04ae6979f061f7eb2ff3d035e02974a38
Kenneth Mead <kennethmead784@gmail.com>
2024-03-17 17:16:35 -0400

    Moved angular resume to its own repo

--- dev/angular/src/app/models/CommuteType.ts
+++
@@ -1,1 +0,0 @@
-export type Commute = "Remote" | "Onsite" | "Hybrid";
--- dev/angular/src/app/models/InfoEntry.ts
+++
@@ -1,7 +0,0 @@
-import { DateTime } from "luxon"
-
-export interface InfoEntry {
-  title: string
-  startDate: DateTime,
-  endDate: DateTime | null,
-}
